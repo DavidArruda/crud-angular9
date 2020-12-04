da06cde248161ddbd8f6e29a7f47eb7fdadd3c02
David <deh0_@hotmail.com>
2020-12-04 19:40:20 -0300

    alterado métodos para trabalhar com paginação de dados

--- src/app/componente/usuario/usuario.component.ts
+++ src/app/componente/usuario/usuario.component.ts
@@ -5,46 +5,60 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 
 
 @Component({
-  selector: 'app-usuario',
-  templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.css']
+  selector: "app-usuario",
+  templateUrl: "./usuario.component.html",
+  styleUrls: ["./usuario.component.css"],
 })
 export class UsuarioComponent implements OnInit {
-
   students: Array<User[]>;
   nome: string;
   total: number;
   p: number = 1;
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
-    this.usuarioService.getStudentList().subscribe(data => {
+    this.usuarioService.getStudentList().subscribe((data) => {
       this.students = data.content;
       this.total = data.totalElements;
     });
   }
 
   deleteUsuario(id: number, index: number) {
-    if (confirm('Deseja remover este usuário ?')) {
-      this.usuarioService.deletarUsuario(id).subscribe(data => {
+    if (confirm("Deseja remover este usuário ?")) {
+      this.usuarioService.deletarUsuario(id).subscribe((data) => {
         this.students.splice(index, 1); // Remove da tela
-
       });
     }
   }
 
   consultarUser() {
-    this.usuarioService.consultarUser(this.nome).subscribe(data => {
-      this.students = data;
-    });
+    if (this.nome === "") {
+      this.usuarioService.getStudentList().subscribe((data) => {
+        this.students = data.content;
+        this.total = data.totalElements;
+      });
+    } else {
+      this.usuarioService.consultarUser(this.nome).subscribe((data) => {
+        this.students = data.content;
+        this.total = data.totalElements;
+      });
+    }
   }
 
   loadPage(page) {
-    this.usuarioService.getStudentListPage(page - 1).subscribe(data => {
-      this.students = data.content;
-      this.total = data.totalElements;
-    });
+    if (this.nome !== '') {
+      this.usuarioService
+        .consultarUserPorPage(this.nome, (page - 1))
+        .subscribe((data) => {
+          this.students = data.content;
+          this.total = data.totalElements;
+        });
+    } else {
+      this.usuarioService.getStudentListPage(page - 1).subscribe((data) => {
+        this.students = data.content;
+        this.total = data.totalElements;
+      });
+    }
   }
-
 }
